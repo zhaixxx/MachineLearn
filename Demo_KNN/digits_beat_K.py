f87f69f5256b9c6929ab04ed335823843a93376d
shiyn <14521677@qq.com>
2019-11-07 15:28:09 +0800

    网格搜索策略

--- Demo_KNN/digits_beat_K.py
+++ Demo_KNN/digits_beat_K.py
@@ -2,6 +2,7 @@ import  numpy as np
 from sklearn import datasets
 from Demo_KNN.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
 
 digits = datasets.load_digits()
 x = digits.data
@@ -15,13 +16,29 @@ best_score = 0.0
 best_k = -1
 best_p = -1
 # 网格搜索策略
-for k in range(1,11):
-    for p in range(1,6):
-        knn_clf = KNeighborsClassifier(n_neighbors=k,weights="distance",p=p)
-        knn_clf.fit(x_train,y_tarin)
-        score = knn_clf.score(x_test,y_test)
-        if score>best_score:
-            best_k = k
-            best_score = score
-            best_p = p
-print(best_score,best_k,best_p)
+# for k in range(1,11):
+#     for p in range(1,6):
+#         knn_clf = KNeighborsClassifier(n_neighbors=k,weights="distance",p=p)
+#         knn_clf.fit(x_train,y_tarin)
+#         score = knn_clf.score(x_test,y_test)
+#         if score>best_score:
+#             best_k = k
+#             best_score = score
+#             best_p = p
+# print(best_score,best_k,best_p)
+#
+knn_clf = KNeighborsClassifier()
+praram_grid = [
+    {
+        'weights':['uniform'],
+        'n_neighbors':[i for i in range (1,11)]
+    },
+    {
+        'weights':['distance'],
+        'n_neighbors':[i for i in range (1,11)],
+        'p':[i for i in range(1,6)]
+    }
+]
+grid_search = GridSearchCV(knn_clf,praram_grid)
+grid_search.fit(x_train,y_tarin)
+print(grid_search.best_estimator_)
